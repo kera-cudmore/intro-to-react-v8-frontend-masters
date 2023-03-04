db80e57c29c66070372759815f20182ad446bca8
Kera Cudmore <kera.cudmore@hotmail.com>
2023-03-04 23:57:47 +0000

    add: npm & prettier lessons

--- adopt_me/src/App.js
+++ adopt_me/src/App.js
@@ -4,33 +4,29 @@ const Pet = (props) => {
     React.createElement("h1", {}, props.name),
     React.createElement("h2", {}, props.animal),
     React.createElement("h2", {}, props.breed),
-  ])
+  ]);
 };
 
 // creating App function
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
+  return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
     React.createElement(Pet, {
       animal: "Dog",
       name: "Bubba",
-      breed: "English Bulldog"
-  }),
+      breed: "English Bulldog",
+    }),
     React.createElement(Pet, {
       animal: "Dog",
       name: "Spud",
-      breed: "Jack Russell"
-  }),
+      breed: "Jack Russell",
+    }),
     React.createElement(Pet, {
       animal: "Dog",
       name: "Shona",
-      breed: "Boxer"
-  }),
-    ]  
-    );
+      breed: "Boxer",
+    }),
+  ]);
 };
 
 // instantiating the function
